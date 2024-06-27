ee798f7529b775355961d4856612942c617dee08
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-06-27 13:04:58 +0200

    add: checking if there are answers in results page

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import React, { Suspense } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import Link from "next/link";
 import ResultItem from "@/components/result/result-item";
 import problemsData from "@/data/manager_problems_solutions.json";
-
-const Fallback = () => (
- <div className="flex justify-center items-center h-screen">
-  <p>Loading...</p>
- </div>
-);
 
 const ResultsPage = () => {
  const searchParams = useSearchParams();
@@ -30,26 +24,34 @@ const ResultsPage = () => {
    (_, index) => answers[index]
   );
   setResults(identifiedProblems);
+
+  if (answers.every((answer) => !answer)) {
+  }
  }, [searchParams, problems]);
 
  return (
   <div className="flex flex-col items-center pt-14 min-h-screen h-full gap-4">
    <h2 className="text-blue-950 text-2xl font-bold">Rezultaty</h2>
-   {results.map((result, index) => (
-    <ResultItem
-     key={index}
-     problem={result}
-     solution={solutions[index]}
-    />
-   ))}
+   {results.length > 0 ? (
+    results.map((result, index) => (
+     <ResultItem
+      key={index}
+      problem={result}
+      solution={solutions[index]}
+     />
+    ))
+   ) : (
+    <div className="flex flex-col items-center gap-2">
+     <p>
+      Wygląda na to, że jeszcze nie odpowiedziałeś na żadne pytania.
+     </p>
+     <button className="bg-blue-950 text-xl text-white rounded-full py-4 px-6 font-bold hover:scale-105 hover:bg-blue-900 transition">
+      <Link href="/quiz">Zrób quiz!</Link>
+     </button>
+    </div>
+   )}
   </div>
  );
 };
 
-const ResultsPageWithSuspense = () => (
- <Suspense fallback={<Fallback />}>
-  <ResultsPage />
- </Suspense>
-);
-
-export default ResultsPageWithSuspense;
+export default ResultsPage;
