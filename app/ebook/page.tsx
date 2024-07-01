e9cfaf20afb2e52bafe7c5b0da94068929ca19df
jakub-bartoszek-developer <k.bartoszek507@wp.pl>
2024-07-01 17:28:10 +0200

    fixed path

--- app/ebook/page.tsx
+++ app/ebook/page.tsx
@@ -6,7 +6,7 @@ const Ebook = () => {
   <div className="flex items-center justify-center h-full p-4 min-h-screen">
    <button className="bg-blue-950 text-4xl text-white rounded-full p-6 font-bold hover:scale-105 hover:bg-blue-900 transition">
     <a
-     href={`${prefix}Społeczeństwo informacyjne-wyzwania.pdf`}
+     href={`${prefix}/Społeczeństwo informacyjne-wyzwania.pdf`}
      download="Społeczeństwo informacyjne-wyzwania"
     >
      Pobierz
